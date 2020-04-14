e9656e08b89582303d01004c4dfc31e62e37c437
clementine-rochut <60394461+clementine-rochut@users.noreply.github.com>
2020-04-14 14:29:16 +0200

    New programme couleurs

--- Programme_couleurs.py
+++ Programme_couleurs.py
@@ -1,24 +1,22 @@
-couleur=str(input("Entrez la couleur du vêtement : "))
-if couleur == noir:
+couleur=input("Entrez la couleur du vêtement : ")
+if couleur == "noir":
     print("Vous pouvez ajouter des pièces de n'importe quelle couleur.")
-if couleur == blanc:
+if couleur == "blanc":
     print("Vous pouvez ajouter des pièces de n'importe quelle couleur.")
-if couleur == rouge :
+if couleur == "rouge" :
     print("Vous pouvez ajouter des pièces noires ou blanches.")
-if couleur == jean:
+if couleur == "jean":
     print("Vous pouvez ajouter des pièces de n'importe quelle couleur sauf du jean.")
-if couleur == marine:
+if couleur == "marine":
     print("Evitez d'ajouter des pièces noires.")
-if couleur == vert:
+if couleur == "vert":
     print("Evitez d'ajouter des pièces rouges.")
-if couleur == orange:
+if couleur == "orange":
     print("Evitez d'ajouter des pièces roses.")
-if couleur == violet:
+if couleur == "violet":
     print("Evitez d'ajouter des pièces jaunes.")
-if couleur == beige:
+if couleur == "beige":
     print("Vous pouvez ajouter des pièces noires ou blanches.")
-if couleur == doré:
+if couleur == "doré":
     print("Ne pas ajouter de pièces argenteés.")
-    
-    
     
