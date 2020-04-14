8bb20496608b3f5936b4f3c718dc6dacbd829f24
clementine-rochut <60394461+clementine-rochut@users.noreply.github.com>
2020-04-14 14:59:10 +0200

    fonction choixCouleur

---
+++ fonction choixCouleurs.py
@@ -0,0 +1,21 @@
+def choixCouleur(couleur) :
+   if couleur == "noir":
+    affichageCouleur.configure(text="Vous pouvez ajouter des pièces de n'importe quelle couleur.")
+   if couleur == "blanc":
+    affichageCouleur.configure(text="Vous pouvez ajouter des pièces de n'importe quelle couleur.")
+   if couleur == "rouge" :
+    affichageCouleur.configure(text="Vous pouvez ajouter des pièces noires ou blanches.")
+   if couleur == "jean":
+    affichageCouleur.configure(text="Vous pouvez ajouter des pièces de n'importe quelle couleur sauf du jean.")
+   if couleur == "marine":
+    affichageCouleur.configure(text="Evitez d'ajouter des pièces noires.")
+   if couleur == "vert":
+    affichageCouleur.configure(text="Evitez d'ajouter des pièces rouges.")
+   if couleur == "orange":
+    affichageCouleur.configure(text="Evitez d'ajouter des pièces roses.")
+   if couleur == "violet":
+    affichageCouleur.configure(text="Evitez d'ajouter des pièces jaunes.")
+   if couleur == "beige":
+    affichageCouleur.configure(text="Vous pouvez ajouter des pièces noires ou blanches.")
+   if couleur == "doré":
+    affichageCouleur.configure(text="Ne pas ajouter de pièces argenteés.")
